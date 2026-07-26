6bf0274f068126a896d5aa965cb0a2ca6d09ce7b
nexuusdev <mamefat2004@gmail.com>
2026-07-26 15:31:25 +0000

    mysql to postgres

--- config/database.php
+++ config/database.php
@@ -1,10 +1,12 @@
 <?php
 
+// Les variables du .env sont chargées par phpdotenv (createImmutable) dans $_ENV.
+// Attention : getenv() ne fonctionne PAS ici, phpdotenv 5 n'alimente pas putenv() par défaut.
 return [
-    'host'     => getenv('DB_HOST'),
-    'port'     => getenv('DB_PORT') ?: '5432',
-    'db_name'  => getenv('DB_NAME'),
-    'username' => getenv('DB_USERNAME'),
-    'password' => getenv('DB_PASSWORD'),
-    'charset'  => getenv('DB_CHARSET') ?: 'utf8'
+    'host'     => $_ENV['DB_HOST'] ?? 'localhost',
+    'port'     => $_ENV['DB_PORT'] ?? '5432',
+    'db_name'  => $_ENV['DB_NAME'] ?? '',
+    'username' => $_ENV['DB_USERNAME'] ?? '',
+    'password' => $_ENV['DB_PASSWORD'] ?? '',
+    'charset'  => $_ENV['DB_CHARSET'] ?? 'utf8'
 ];
